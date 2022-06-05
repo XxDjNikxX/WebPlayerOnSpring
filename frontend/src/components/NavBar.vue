8974856ac62776434fd7ad5c910b1fea816670df
DjNik <nikitos1235@gmail.com>
2022-06-05 19:31:29 +0300

    Added border to white version, removed buttons

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
-    <nav class="shadow-lg  dark:bg-slate-800">
-        <div class="max-w-6xl mx-auto px-4  bg-purple-800 text-black dark:text-white">
+    <nav class="shadow-lg  dark:bg-slate-800 border-b-4 border-indigo-500 dark:border-b-black">
+        <div class="max-w-6xl mx-auto px-4  bg-purple-800  text-black dark:text-white">
             <div class="flex justify-between">
                 <div class="flex space-x-7">
                     <div>
                         <router-link to="/">
-                            <a class="flex items-center py-4  ">
+                            <a class="flex items-center py-4 ">
                                 <img src="../assets/logo_transparent.png" alt="Logo" class="box-border bg-slate-100 w-20 h-20" />
                                 <span
                                     class="font-body hover:text-indigo-500  border-indigo-500 border-b-4 transition duration-300 text-lg">Runier
@@ -27,12 +27,12 @@
                         </router-link>
                     </div>
                 </div>
-                <div class="hidden md:flex items-center space-x-3 ">
+                <!-- <div class="hidden md:flex items-center space-x-3 ">
                     <a href=""
                         class="py-2 px-2 pr-5 pl-5 font-medium text-white rounded bg-indigo-500 hover:bg-indigo-400 hover:text-white transition duration-300">Войти</a>
                     <a href=""
                         class="py-2 px-2 pr-5 pl-5 font-medium text-white bg-indigo-500 rounded hover:bg-indigo-400 transition duration-300">Регистрация</a>
-                </div>
+                </div> -->
             </div>
         </div>
     </nav>
