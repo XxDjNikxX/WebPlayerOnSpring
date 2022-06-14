3fd17489b6b89cc3717e4401f92376bd6fbfa3bb
Voroncov <nikitos1235@gmail.com>
2022-06-14 17:19:04 +0300

    Fuck, favicon errors

--- frontend/src/components/AboutPage.vue
+++ frontend/src/components/AboutPage.vue
@@ -1,7 +1,7 @@
 <template>
 <section class="about">
- <div class="w-full font-body relative  text-center bg-indigo-100 dark:bg-indigo-400 ">
-    <div class="flex items-center justify-center h-screen bg-white dark:bg-indigo-400">
+ <div class="w-full font-body text-center bg-indigo-200 dark:bg-indigo-400 ">
+    <div class="flex items-center justify-center h-screen  dark:bg-indigo-400">
       <div class="bg-slate-200 shadow-lg rounded-md w-auto h-auto p-8 items-center  dark:bg-slate-800 dark:text-white">
           Этот хороший плеер написан студентом из 3802 группы
       </div>
